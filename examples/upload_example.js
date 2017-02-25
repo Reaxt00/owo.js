492f71f29b70e61dd390d461bc47879c486d8cb3
Reaxt00 <koehlertrevor@gmail.com>
2017-02-25 16:24:25 -0500

    Made it good
    improved example with good

--- examples/upload_example.js
+++ examples/upload_example.js
@@ -4,11 +4,39 @@
 // |___|_____|___|_|_| |___|
 //                 |___|
 //
-
+const dean = "meme"
 // Import OwO.JS for use.
 const owo = new (require('owo.js'))('YOUR-KEY-HERE');
 
-// Upload the file to OwO.
-owo.upload('icon.png')
+// Upload the file to OwO
+if(true === true) {
+ if(true === true) {
+  if(true === true) {
+    if (false === true) {
+     console.log("ur bad")   
+    } else if (true === true) {
+     if (dean === "meme"){
+         console.log(dean)
+         console.log(true)
+        console.log(owo)
+        owo.upload('dean.png')
+     .then(data => console.log(data))
+    .catch(err => console.log(err));
+    }
+  }
+ }
+}
+
+//catch error
     .then(data => console.log(data))
     .catch(err => console.log(err));
+//do thing
+
+
+
+
+
+
+
+
+console.log("magic")
